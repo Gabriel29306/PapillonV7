11e139a6bf846695594abb2006a12a62c163bdbb
Gabriel29306 <ggkervran@outlook.com>
2025-01-25 22:53:29 +0100

    lint

--- src/components/Settings/ReelGallery.tsx
+++ src/components/Settings/ReelGallery.tsx
@@ -133,7 +133,7 @@ const ReelGallery = ({ reels }: ReelGalleryProps) => {
       {selectedReel && (
         <GradeModal
           isVisible={!!selectedReel}
-          imageBase64={selectedReel.image || ""}
+          reel={selectedReel}
           onClose={() => setSelectedReel(null)}
           DeleteGrade={() => selectedReel.id && deleteReel(selectedReel.id)}
         />
